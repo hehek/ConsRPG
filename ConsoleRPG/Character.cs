fae48b832229099718ca6e1961f0d7a898066d1a
Anna Pavlovskaya <annvsann@gmail.com>
2019-02-07 21:29:54 +0200

    +interface WeaponBehavior

---
+++ ConsoleRPG/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG
+{
+    public abstract class Character
+    {
+        string name;
+        WeaponBehavior weapon;
+
+        public abstract void display();    
+        public void Attac()
+        {
+            weapon.Attac();
+        }     
+        public void setWeapon(WeaponBehavior weapon)
+        {
+            this.weapon = weapon;
+        }
+    }
+       
+
+}
---
+++ ConsoleRPG/WheaponBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG
+{
+    public interface WeaponBehavior
+    {
+        void Attac();
+    }
+
+    class BowBehavior : WeaponBehavior
+    {
+        int maxDamage = 50;
+        int minDamage = 10;
+        Random rand = new Random();
+
+        public void Attac()
+        {            
+            Console.WriteLine("Weapon: Bow \nDamage: {0}", rand.Next(minDamage,maxDamage));
+        }
+    }
+
+    class SwardBehavior : WeaponBehavior
+    {
+        int maxDamage = 55;
+        int minDamage = 30;
+        Random rand = new Random();
+
+        public void Attac()
+        {
+            Console.WriteLine("Weapon: Sward \nDamage: {0}", rand.Next(minDamage, maxDamage));
+        }
+    }
+
+    class StickBehavior : WeaponBehavior
+    {
+        int maxDamage = 60;
+        int minDamage = 5;
+        Random rand = new Random();
+
+        public void Attac()
+        {
+            Console.WriteLine("Weapon: Stick \nDamage: {0}", rand.Next(minDamage, maxDamage));
+        }
+    }
+
+
+}
